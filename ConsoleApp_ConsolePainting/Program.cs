0cfb1351a4f1240087849cc4c3a9516360696efd
Yervand.Marikyan <yervand.marikyan@gmail.com>
2021-07-05 17:15:53 +0400

    first commit

---
+++ ConsoleApp_ConsolePainting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp_ConsolePainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+
+            while (true)
+            {
+                ConsoleColor color = (ConsoleColor)rd.Next(16);
+                BackgroundColor = color;
+                Clear();
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
